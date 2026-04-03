e164a0633a011a2eb962b9b963a9676934154e30
77tor <77tor@ikrs.no>
2026-04-04 00:59:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -285,7 +285,7 @@ oppsett.oppgaver.forEach(o => {
 });
 
 // ENDRET HER: Bruker <span> med display:block og !important for å sikre synlighet ved utskrift
-    hode += `<th style="text-align:center; vertical-align:middle; min-width:60px;">SUM<br>Kr: ${oppsett.grenseTotal}</th><th class="no-print">Handling</th></tr>`;
+    hode += `<th style="text-align:center;">Sum<br><small>(Kritisk: ≤${oppsett.grenseTotal})</small></th><th class="no-print">Handling</th></tr>`;
 
 tHead.innerHTML = hode;
 
