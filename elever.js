4128c1ecdb849e60143900b8adfd29cc2ef6ef74
77tor <77tor@ikrs.no>
2026-09-18 12:17:28 +0200

    Add files via upload

--- elever.js
+++ elever.js
@@ -173,6 +173,7 @@ window.elevRegister = {
     "Rosen, Yohan Andresen": { startTrinn: 1, startKlasse: 'C', startAar: 2024, sluttAar: 2031 },
     "Vinknes, Franki Thurgood": { startTrinn: 1, startKlasse: 'C', startAar: 2024, sluttAar: 2031 },
     "Vu, Matheo Hieu": { startTrinn: 1, startKlasse: 'C', startAar: 2024, sluttAar: 2031 },
+    "Belayhun, Wudasie Endalk": { startTrinn: 3, startKlasse: 'C', startAar: 2026, sluttAar: 2031 },
 
 // 2. TRINN (Startet 2024)
     "Alieksieiev, Artem": { startTrinn: 2, startKlasse: 'A', startAar: 2024, sluttAar: 2030 },
